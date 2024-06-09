19c563a2581142f438ddae2f947e867e48aad5b7
JiancongZhu0423 <jiancoz@uci.edu>
2024-06-09 13:14:26 -0700

    Create queryReq.ftl template

--- member/src/main/java/com/jiawa/train/member/req/PassengerSaveReq.java
+++ member/src/main/java/com/jiawa/train/member/req/PassengerSaveReq.java
@@ -20,31 +20,31 @@ public class PassengerSaveReq {
     /**
     * 姓名
     */
-                @NotBlank(message = "【姓名】不能为空")
+    @NotBlank(message = "【姓名】不能为空")
     private String name;
 
     /**
     * 身份证
     */
-                @NotBlank(message = "【身份证】不能为空")
+    @NotBlank(message = "【身份证】不能为空")
     private String idCard;
 
     /**
     * 旅客类型|枚举[PassengerTypeEnum]
     */
-                @NotBlank(message = "【旅客类型】不能为空")
+    @NotBlank(message = "【旅客类型】不能为空")
     private String type;
 
     /**
     * 新增时间
     */
-        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
 
     /**
     * 修改时间
     */
-        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date updateTime;
 
     public Long getId() {
