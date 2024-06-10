47064c965a3ebc8f6697ba95b7408043e1671c80
JiancongZhu0423 <jiancoz@uci.edu>
2024-06-09 20:43:33 -0700

    Fix some tiny problems of passenger.vue file

--- generator/src/main/java/com/jiawa/train/generator/gen/EnumGenerator.java
+++ generator/src/main/java/com/jiawa/train/generator/gen/EnumGenerator.java
@@ -4,13 +4,12 @@ package com.jiawa.train.generator.gen;
 import cn.hutool.core.util.StrUtil;
 import com.jiawa.train.member.enums.PassengerTypeEnum;
 
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 import java.lang.reflect.Method;
 
 public class EnumGenerator {
-    static String path = "web" + File.separator + "src" + File.separator + "assets" + File.separator + "js" + File.separator + "enums.js";
+    static String path = "web/src/assets/js/enums.js";
 
     public static void main(String[] args) {
         StringBuffer bufferObject = new StringBuffer();
