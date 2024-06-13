bff998b154f0aac015a8b882490e3aaf24095866
JiancongZhu0423 <jiancoz@uci.edu>
2024-06-12 22:33:01 -0700

    Added the function of generating daily train trips

--- business/src/main/java/com/jiawa/train/business/controller/admin/DailyTrainAdminController.java
+++ business/src/main/java/com/jiawa/train/business/controller/admin/DailyTrainAdminController.java
@@ -1,15 +1,17 @@
 package com.jiawa.train.business.controller.admin;
 
-import com.jiawa.train.common.context.LoginMemberContext;
-import com.jiawa.train.common.resp.CommonResp;
-import com.jiawa.train.common.resp.PageResp;
 import com.jiawa.train.business.req.DailyTrainQueryReq;
 import com.jiawa.train.business.req.DailyTrainSaveReq;
 import com.jiawa.train.business.resp.DailyTrainQueryResp;
 import com.jiawa.train.business.service.DailyTrainService;
+import com.jiawa.train.common.resp.CommonResp;
+import com.jiawa.train.common.resp.PageResp;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/admin/daily-train")
@@ -36,4 +38,11 @@ public class DailyTrainAdminController {
         return new CommonResp<>();
     }
 
+    @GetMapping("/gen-daily/{date}")
+    public CommonResp<Object> genDaily(@PathVariable("date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date date) {
+        dailyTrainService.genDaily(date);
+        return new CommonResp<>();
+    }
+
 }
+
